e51563d92aa5f01dfc72450d273da75c641414c5
Hedi-hub <72658290+Hedi-hub@users.noreply.github.com>
2020-12-10 20:15:45 +0100

    intial commit plus added index and style file

---
+++ src/main/java/cookingguide/CookingGuideApplication.java
@@ -0,0 +1,13 @@
+package cookingguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingGuideApplication.class, args);
+    }
+
+}
